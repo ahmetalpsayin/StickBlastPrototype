42de9c30dc3eeb4c7d452568f1b1d746e38cd9c5
AHMET ALP SAYIN <ahmetalpsayin@hotmail.com>
2025-03-24 19:26:45 +0300

    rotated 180 U L sticks

--- Assets/Scripts/StickSpawner.cs
+++ Assets/Scripts/StickSpawner.cs
@@ -7,9 +7,9 @@ public class StickSpawner : MonoBehaviour
     public GameObject iStickPrefab;
     public GameObject lStickPrefab;
     public GameObject uStickPrefab;
-    public GameObject iStickPrefab_rotated90;
-    public GameObject lStickPrefab_rotated90;
-    public GameObject uStickPrefab_rotated90;
+    public GameObject iStickPrefab_rotated180;
+    public GameObject lStickPrefab_rotated180;
+    public GameObject uStickPrefab_rotated180;
 
     public Transform selectionArea;  // SelectionArea GameObject
     public Vector3[] spawnPositions; // Selection alanýnda 3 pozisyon
@@ -25,7 +25,7 @@ public class StickSpawner : MonoBehaviour
     {
         ClearSelectionArea();
 
-        GameObject[] pool = new GameObject[] { iStickPrefab, lStickPrefab, uStickPrefab , iStickPrefab_rotated90 };
+        GameObject[] pool = new GameObject[] { iStickPrefab, lStickPrefab, uStickPrefab , iStickPrefab_rotated180, lStickPrefab_rotated180, uStickPrefab_rotated180 };
 
         for (int i = 0; i < 3; i++)
         {
